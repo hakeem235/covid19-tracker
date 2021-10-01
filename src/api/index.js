3e70fff265e3f1e8d2637f2cf16e5f18db96b422
Ahmed Hakeem <ahmedhakeem@Ahmeds-MacBook-Pro.local>
2021-10-01 11:32:42 -0400

    finsh from the card and adding the line chart

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const url = "https://covid19.mathdro.id/api";
+
+export const fetchData = async () => {
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetcDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    return data.map(({ confirmed, deaths, reportDate: date }) => ({
+      confirmed: confirmed.total,
+      deaths: deaths.total,
+      date,
+    }));
+  } catch (errorr) {}
+};
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CountryPicker() {
+    return (
+        <div>
+           
+        </div>
+    )
+}
+
+export default CountryPicker
